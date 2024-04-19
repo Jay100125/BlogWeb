2e17e8e84eeb1d22127861d18383a0b802832889
Jay100125 <jaypatel100125@gmail.com>
2024-04-19 19:57:34 +0530

    changes

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -1,6 +1,4 @@
 "use client";
-
-// import { ThemeContext } from "@/context/ThemeContext";
 import { ThemeContext } from "./../context/ThemeContext"
 import React, { useContext, useEffect, useState } from "react";
 
